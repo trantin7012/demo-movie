0b26fc1712d65c6a85fb4f144e69ef5708bba957
Tin Tin <trantin7012@gmail.com>
2020-11-24 12:37:41 +0700

    Updates

---
+++ precache-manifest.cd38cf069b13d433b0b901cea2a9fe4f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bffecf63f8a3acb501347ad114cb5ca",
+    "url": "/demo-react-movie/index.html"
+  },
+  {
+    "revision": "a01362f29c74bccf0458",
+    "url": "/demo-react-movie/static/css/2.7fd3841b.chunk.css"
+  },
+  {
+    "revision": "4c5c23b636a68de60f20",
+    "url": "/demo-react-movie/static/css/main.cd380b24.chunk.css"
+  },
+  {
+    "revision": "a01362f29c74bccf0458",
+    "url": "/demo-react-movie/static/js/2.c3bba4d6.chunk.js"
+  },
+  {
+    "revision": "2a1825e485c0b8c40364fdec5e88cf0a",
+    "url": "/demo-react-movie/static/js/2.c3bba4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c5c23b636a68de60f20",
+    "url": "/demo-react-movie/static/js/main.132cef75.chunk.js"
+  },
+  {
+    "revision": "d0f35db48cb62448b765",
+    "url": "/demo-react-movie/static/js/runtime-main.23618442.js"
+  },
+  {
+    "revision": "4c4a3b7b4af30fa742d2794a75d3196f",
+    "url": "/demo-react-movie/static/media/bg2.4c4a3b7b.jpg"
+  },
+  {
+    "revision": "4c4a3b7b4af30fa742d2794a75d3196f",
+    "url": "/demo-react-movie/static/media/cat.4c4a3b7b.jpg"
+  },
+  {
+    "revision": "714b42c777bb90383b3de8cda145cee0",
+    "url": "/demo-react-movie/static/media/film.714b42c7.mp4"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/demo-react-movie/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/demo-react-movie/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/demo-react-movie/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/demo-react-movie/static/media/slick.f97e3bbf.svg"
+  }
+]);
