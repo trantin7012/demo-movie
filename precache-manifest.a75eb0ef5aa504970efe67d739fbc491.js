972eacf40a2e036cb9f28bf54b17a80e4a2e2b07
Tin Tin <trantin7012@gmail.com>
2020-11-24 12:59:16 +0700

    Updates

---
+++ precache-manifest.a75eb0ef5aa504970efe67d739fbc491.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abec37e1c1311b48063ef8caa960c347",
+    "url": "/demo-react-movie/index.html"
+  },
+  {
+    "revision": "a01362f29c74bccf0458",
+    "url": "/demo-react-movie/static/css/2.7fd3841b.chunk.css"
+  },
+  {
+    "revision": "d06a67179587c3827fd9",
+    "url": "/demo-react-movie/static/css/main.cd380b24.chunk.css"
+  },
+  {
+    "revision": "a01362f29c74bccf0458",
+    "url": "/demo-react-movie/static/js/2.c3bba4d6.chunk.js"
+  },
+  {
+    "revision": "2a1825e485c0b8c40364fdec5e88cf0a",
+    "url": "/demo-react-movie/static/js/2.c3bba4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d06a67179587c3827fd9",
+    "url": "/demo-react-movie/static/js/main.020d5881.chunk.js"
+  },
+  {
+    "revision": "d0f35db48cb62448b765",
+    "url": "/demo-react-movie/static/js/runtime-main.23618442.js"
+  },
+  {
+    "revision": "4c4a3b7b4af30fa742d2794a75d3196f",
+    "url": "/demo-react-movie/static/media/bg2.4c4a3b7b.jpg"
+  },
+  {
+    "revision": "4c4a3b7b4af30fa742d2794a75d3196f",
+    "url": "/demo-react-movie/static/media/cat.4c4a3b7b.jpg"
+  },
+  {
+    "revision": "714b42c777bb90383b3de8cda145cee0",
+    "url": "/demo-react-movie/static/media/film.714b42c7.mp4"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/demo-react-movie/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/demo-react-movie/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/demo-react-movie/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/demo-react-movie/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie/precache-manifest.87edf7712b78586990bc8038478df820.js"
+  "/demo-react-movie/precache-manifest.a75eb0ef5aa504970efe67d739fbc491.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demo-react-movie/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
