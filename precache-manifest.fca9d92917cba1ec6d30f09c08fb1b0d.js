d45ae58f3e356e7fa9ca1fa6281977345ec2b169
Tin Tin <trantin7012@gmail.com>
2020-11-24 12:28:58 +0700

    Updates

---
+++ precache-manifest.fca9d92917cba1ec6d30f09c08fb1b0d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce1693952c98c495f506ff0fbb0d2e5",
+    "url": "/deploy-github/index.html"
+  },
+  {
+    "revision": "a46921ae323129631e84",
+    "url": "/deploy-github/static/css/2.e490c2d4.chunk.css"
+  },
+  {
+    "revision": "4ced52d2514753b29fc8",
+    "url": "/deploy-github/static/css/main.3eae2efe.chunk.css"
+  },
+  {
+    "revision": "a46921ae323129631e84",
+    "url": "/deploy-github/static/js/2.c3bba4d6.chunk.js"
+  },
+  {
+    "revision": "2a1825e485c0b8c40364fdec5e88cf0a",
+    "url": "/deploy-github/static/js/2.c3bba4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ced52d2514753b29fc8",
+    "url": "/deploy-github/static/js/main.affa9c2d.chunk.js"
+  },
+  {
+    "revision": "a9b7c87ef6486c44dc08",
+    "url": "/deploy-github/static/js/runtime-main.ec65392f.js"
+  },
+  {
+    "revision": "4c4a3b7b4af30fa742d2794a75d3196f",
+    "url": "/deploy-github/static/media/bg2.4c4a3b7b.jpg"
+  },
+  {
+    "revision": "4c4a3b7b4af30fa742d2794a75d3196f",
+    "url": "/deploy-github/static/media/cat.4c4a3b7b.jpg"
+  },
+  {
+    "revision": "714b42c777bb90383b3de8cda145cee0",
+    "url": "/deploy-github/static/media/film.714b42c7.mp4"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/deploy-github/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/deploy-github/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/deploy-github/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/deploy-github/static/media/slick.f97e3bbf.svg"
+  }
+]);
